261a7ad4a80d5ff6b20e7d3c76cec2b9319535e1
SimSalahBim <sim.salah.bim@t-online.de>
2015-05-27 17:12:56 +0200

    Create funktionen.java

---
+++ src/func/funktionen.java
@@ -0,0 +1,54 @@
+package func;
+
+import javax.swing.JOptionPane;
+
+public class funktionen {
+
+	public static boolean regestrieren(String name,String mail, char[] password, char[] password_w){
+		if(name.equals("")){
+			JOptionPane.showConfirmDialog(null,"Bitte Name eingeben",null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+			return false;
+		} else {
+			if(password.equals("")){
+				JOptionPane.showConfirmDialog(null,"Bitte Password eingeben",null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+				return false;
+			}else{
+				if(password.equals(password_w) == true){
+					JOptionPane.showConfirmDialog(null,"Passwörter stimmen nicht überein",null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+					return false;
+				}else{
+//					if(serverrückmeldung) == true){
+						JOptionPane.showConfirmDialog(null, "\"" + name + "\"" + " wurde erfolgreich Registriert!", "", JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+						return true;
+//					} else {
+//						JOptionPane.showConfirmDialog(null,"Fehler bei der Regestrierung",null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+//						return false;
+//					}
+				}
+			}
+		}
+	}		
+
+	public static boolean login(String name, char[] password){
+//		if(serverrückmeldung == true){
+		return true;
+//		} else {
+//		JOptionPane.showConfirmDialog(null,"Fehler bei der Regestrierung",null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+//		return false;		
+//		}
+	}
+
+	public static void send(String nachricht){
+		JOptionPane.showConfirmDialog(null, "\"" + nachricht + "\"   " + "Gesendet!", "", JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static void gruppe_aktualisieren(){
+		//
+	}
+	
+	public static void nachrichten_aktualisieren(){
+		//
+	}
+	
+	
+}
