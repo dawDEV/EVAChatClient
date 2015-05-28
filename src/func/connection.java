3d105ef01c2c7a4a7dacd03a99c205a595e66d17
thejacko12354 <julian.meisel@online.de>
2015-05-28 11:55:37 +0200

    update

--- src/func/connection.java
+++ src/func/connection.java
@@ -1,6 +1,6 @@
 package func;
 
-<<<<<<< HEAD
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,57 +8,47 @@ import java.io.PrintWriter;
 import java.net.*;
 
 public class connection{
-		private Socket mSocket;
-		private BufferedReader mInput;
-		private PrintWriter mOutput;
+	private Socket mSocket;
+	private BufferedReader mInput;
+	private PrintWriter mOutput;
 	 	
 		
-		public void connectToServer(String mServAddress, int port){
-			try{
-				
-				mSocket = new Socket(mServAddress, port);
-				System.out.println("Connected to "+mServAddress+" on port "+port);
-				
-				mInput = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
-				mOutput = new PrintWriter(mSocket.getOutputStream());
-					
-				
-			}catch(UnknownHostException e){
-				e.printStackTrace();
-				System.err.println("Unknown host!");
-			}catch (IOException e) {
-				
-				e.printStackTrace();
-				System.err.println("Stream Error!");
-			}
+	public void connectToServer(String mServAddress, int port){
+		try{
 			
+			mSocket = new Socket(mServAddress, port);
+			System.out.println("Connected to "+mServAddress+" on port "+port);
 			
+			mInput = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+			mOutput = new PrintWriter(mSocket.getOutputStream());
+				
+			
+		}catch(UnknownHostException e){
+			e.printStackTrace();
+			System.err.println("Unknown Host!");
+		}catch (IOException e) {
+			
+			e.printStackTrace();
+			System.err.println("Stream Error!");
 		}
-		
-		void sendMessage(String msg){
+				
+	}
+	
+	void sendMessage(String msg){
 			mOutput.print(msg);
 			mOutput.flush();
 			System.out.println("client>" + msg);
 		}
-=======
-import javax.swing.JOptionPane;
 
-public class connection {
->>>>>>> origin/master
-
-	public static void connection_aufbauen(){
+	
+	public static void closeConnection(){
 		//
-		JOptionPane.showConfirmDialog(null,"Verbindung zu Server aufgebaut", null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+		System.out.println("Connection Closed!");
 	}
 	
-	public static void connection_abbauen(){
+	public static void con_sondereingabe(String msg){
 		//
-		JOptionPane.showConfirmDialog(null,"Verbindung zu Server abgebaut", null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
-	}
-	
-	public static void con_sondereingabe(String nachricht){
-		//
-		JOptionPane.showConfirmDialog(null,"Nachricht an Server " + nachricht,null, JOptionPane.DEFAULT_OPTION,JOptionPane.INFORMATION_MESSAGE);
+		
 	}
 	
 	public static void con_gruppe_aktualisieren(){
@@ -72,4 +62,5 @@ public class connection {
 	public static void con_gruppe_wechseln(){
 		//
 	}
+}
 	
